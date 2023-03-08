0803a03d179ad349edf49177003535cd58691ae4
Mateo Ortega <mateo041ortega@gmail.com>
2023-03-07 23:12:19 -0700

    added response

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -6,3 +6,7 @@ pub mod query_string;
 pub mod request;
 pub use query_string::{QueryString, Value as QueryStringValue};
 //add all the packages into a package
+pub mod response;
+pub use response::Response;
+pub mod status_code;
+pub use status_code::StatusCode;
